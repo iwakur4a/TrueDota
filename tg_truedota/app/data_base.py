bb8d9fedabee870ba07420a8abda6b1a3ac06300
iwakur4a <iwakura.code@gmail.com>
2026-03-03 12:06:37 +0200

    Обновить функцию уведомления о новом пользователе, добавив дополнительные поля и изменив обработчик вставки записи в базу данных

--- tg_truedota/app/data_base.py
+++ tg_truedota/app/data_base.py
@@ -5,15 +5,27 @@ from app.users_id import read_user_id
 
 
 async def get_db(supabase: AsyncClient, bot: Bot):
-    async def notify_all(bot, username):  # отправка нового логина в чат с ботом
+    async def notify_all(bot, email, nickname, rank_value, role, about):  # отправка нового логина в чат с ботом
+        text = f"""Зарегистрирован новый пользователь
+👤Ник: {nickname}
+📧Почта: {email}
+🏅Ранг: {rank_value}
+🎯Роль: {role}
+🗣О себе: {about}"""
         users_id = read_user_id()
-            for user_id in users_id:
-                await bot.send_message(chat_id=int(user_id.strip()), text=f"Новый логин: {username}")
+        for user_id in users_id:
+            await bot.send_message(chat_id=int(user_id),
+                                   text=text)
 
     def handle_record_inserted(payload):  #
         new_record = payload.get("data", {}).get("record", {})
-        username = new_record.get("username", "неизвестен")
-        asyncio.create_task(notify_all(bot, username))
+        email = new_record.get("email", "неизвестен")
+        nickname = new_record.get("nickname", "неизвестен")
+        rank_value = new_record.get("rank_value", "неизвестен")
+        role = new_record.get("role", "неизвестна")
+        about = new_record.get("about", " ")
+
+        asyncio.create_task(notify_all(bot, email, nickname, rank_value, role, about))
 
     channel = supabase.channel("RunTimeToken")  # подключение к каналу бд
     channel.on_postgres_changes(
@@ -24,32 +36,3 @@ async def get_db(supabase: AsyncClient, bot: Bot):
     )
     await channel.subscribe()
     return channel
-
-
-async def get_register_time(supabase: AsyncClient):  # получение времени регистрации для сортировки
-    register_time = await supabase.table('profiles').select('updated_at').execute()
-    return register_time
-
-
-async def get_last_time(supabase: AsyncClient):  # получение последнего логина иссходя из времени
-    result = await get_register_time(supabase)
-    register_time = result.data
-
-    if not register_time:
-        return None
-
-    times = [row["updated_at"] for row in register_time]
-    times.sort()
-    last_login = times[-1]
-
-    result = await (
-        supabase.table("profiles")
-        .select("username")
-        .eq("updated_at", last_login)
-        .execute()
-    )
-
-    if not result.data:
-        return None
-
-    return result.data[-1]["username"]
